159a0d3b10155e26894e8eaf4a75f1b7d211df0e
Alexander Gugel <alexander.gugel@gmail.com>
2015-04-04 22:48:38 -0700

    Format

--- daemon_test.go
+++ daemon_test.go
@@ -1,65 +1,64 @@
 package bane
 
 import (
-    "testing"
-    "net"
+	"net"
+	"testing"
 )
 
 func TestEcho(t *testing.T) {
-    addr, err := net.ResolveUDPAddr("udp", "localhost:0")
-    if err != nil {
-        t.Fatal(err)
-    }
-    conn, err := net.ListenUDP("udp", addr)
-    if err != nil {
-        t.Fatal(err)
-    }
+	addr, err := net.ResolveUDPAddr("udp", "localhost:0")
+	if err != nil {
+		t.Fatal(err)
+	}
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    d := New(conn, 1000)
+	d := New(conn, 1000)
 
-    realAddr := conn.LocalAddr().(*net.UDPAddr)
+	realAddr := conn.LocalAddr().(*net.UDPAddr)
 
-    d.Out <- &Packet{realAddr, []byte("TestEcho"), nil}
+	d.Out <- &Packet{realAddr, []byte("TestEcho"), nil}
 
-    p := <- d.In
-    if string(p.Data) != "TestEcho" {
-        t.Fatalf("want %v, got %v", string(p.Data), "TestEcho")
-    }
-    p.Data = []byte("TestEchoResponse")
-    d.Out <- p
-    if string(p.Data) != "TestEchoResponse" {
-        t.Fatalf("want %v, got %v", string(p.Data), "TestEchoResponse")
-    }
+	p := <-d.In
+	if string(p.Data) != "TestEcho" {
+		t.Fatalf("want %v, got %v", string(p.Data), "TestEcho")
+	}
+	p.Data = []byte("TestEchoResponse")
+	d.Out <- p
+	if string(p.Data) != "TestEchoResponse" {
+		t.Fatalf("want %v, got %v", string(p.Data), "TestEchoResponse")
+	}
 }
 
 func BenchmarkEcho(b *testing.B) {
-    addr, err := net.ResolveUDPAddr("udp", "localhost:0")
-    if err != nil {
-        b.Fatal(err)
-    }
-    conn, err := net.ListenUDP("udp", addr)
-    if err != nil {
-        b.Fatal(err)
-    }
+	addr, err := net.ResolveUDPAddr("udp", "localhost:0")
+	if err != nil {
+		b.Fatal(err)
+	}
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		b.Fatal(err)
+	}
 
-    d := New(conn, 1000)
+	d := New(conn, 1000)
 
-    realAddr := conn.LocalAddr().(*net.UDPAddr)
+	realAddr := conn.LocalAddr().(*net.UDPAddr)
 
+	go func() {
+		for {
+			d.Out <- &Packet{realAddr, []byte("BenchmarkEcho"), nil}
+		}
+	}()
 
-    go func() {
-        for {
-            d.Out <- &Packet{realAddr, []byte("BenchmarkEcho"), nil}
-        }
-    }()
-
-    n := 0
-    for n < b.N {
-        n++
-        p := <- d.In
-        if string(p.Data) != "BenchmarkEcho" {
-            b.Fatalf("want %v, got %v", string(p.Data), "BenchmarkEcho")
-        }
-        d.Out <- p
-    }
+	n := 0
+	for n < b.N {
+		n++
+		p := <-d.In
+		if string(p.Data) != "BenchmarkEcho" {
+			b.Fatalf("want %v, got %v", string(p.Data), "BenchmarkEcho")
+		}
+		d.Out <- p
+	}
 }
